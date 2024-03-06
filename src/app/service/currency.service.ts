076fb5ce145472921d8a717c0916da07233ae9b2
tom-kotlar <tomaskotlar@yahoo.co.uk>
2024-03-06 11:50:31 +0000

    Ref(*): update

--- src/app/service/currency.service.ts
+++ src/app/service/currency.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+
+interface Currency {
+  code: string;
+  symbol: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
+
+  readonly currencies = signal<Currency[]>([
+    { code: 'USD', symbol: '$' },
+    { code: 'GBP', symbol: '£' },
+    { code: 'EUR', symbol: '€' },
+  ]);
 
   constructor() { }
 }
