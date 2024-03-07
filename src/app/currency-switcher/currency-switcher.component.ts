b8f81a62cd08b7ff1cee3e66f614dbbf7ca95162
tom-kotlar <tomaskotlar@yahoo.co.uk>
2024-03-07 12:07:10 +0000

    Ref(currency-switcher): using forms alternative

--- src/app/currency-switcher/currency-switcher.component.ts
+++ src/app/currency-switcher/currency-switcher.component.ts
@@ -1,11 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { CurrencyService } from '../service/currency.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-currency-switcher',
   templateUrl: './currency-switcher.component.html',
-  styleUrls: ['./currency-switcher.component.scss']
+  styleUrls: ['./currency-switcher.component.scss'], 
+  standalone: true,
+  imports: [FormsModule]
 })
 export class CurrencySwitcherComponent {
   currencyService = inject(CurrencyService);
+
+  // if we use FormsModule
+  selectedCurrency = this.currencyService.currentCurrency().code
 }
