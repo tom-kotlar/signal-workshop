07c323983a1bd1b9cead7c724c3abf5ba5d23027
tom-kotlar <tomaskotlar@yahoo.co.uk>
2024-03-07 12:07:41 +0000

    Ref(currency): 2excercise

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CurrencySwitcherComponent } from '../currency-switcher/currency-switcher.component';
+import { CurrencyService } from '../service/currency.service';
+import { DecimalPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-currency',
   templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.scss']
+  styleUrls: ['./currency.component.scss'],
+  standalone: true,
+  imports: [CurrencySwitcherComponent, JsonPipe, DecimalPipe],
 })
-export class CurrencyComponent {
+export class CurrencyComponent implements OnInit{
+ 
+  currencyService = inject(CurrencyService);
+
+  priceInUsd = 10;
+
+  ngOnInit(): void {
+   console.log(this.currencyService.currentCurrency().code)
+  }
 
 }
